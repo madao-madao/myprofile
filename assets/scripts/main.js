849e30f439d686f9690549451ebf2cd566b11bbd
madao-madao <mamamatao@yandex.ru>
2024-08-22 23:51:43 +0300

    Добавил favicon

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+console.info("Hello");
+alert("Demon"); alert("Marauder");//*В нашем коде может быть несколько инструкций. Инструкции отделяются ";". Например, здесь мы разделили сообщение «Demon Marauder» на два вызова alert:
+const article = document.getElementById("#article1");
+
+
+
