c30961e1272a9cbc7dbe7f6c393093ba46f14955
madao-madao <mamamatao@yandex.ru>
2024-08-23 01:49:34 +0300

    Добавил регистрацию

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,5 @@
 console.info("Hello");
-alert("Demon"); alert("Marauder");//*В нашем коде может быть несколько инструкций. Инструкции отделяются ";". Например, здесь мы разделили сообщение «Demon Marauder» на два вызова alert:
+//alert("Demon"); alert("Marauder");//*В нашем коде может быть несколько инструкций. Инструкции отделяются ";". Например, здесь мы разделили сообщение «Demon Marauder» на два вызова alert:
 const article = document.getElementById("#article1");
 
 
