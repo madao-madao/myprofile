c6c4f5562d45c5137d281588147056fcbc359453
madao-madao <mamamatao@yandex.ru>
2024-09-08 16:39:59 +0300

    Различные исправления, перемещение регистрации

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,3 +6,15 @@ function OpenModal() {
 $("#registration-btn").click(function () {  //подписываемся на событие клика кнопки
     OpenModal()
 });
+function CloseModal() {
+    $("#registration-modal").removeClass("is-active")
+};
+$(document).keyup(function (e) {
+    if (e.keyCode == "27") {
+        CloseModal();
+    }
+});
+$("#close-xmark").on("click", function () {  //обращаемся к крестику и при клике закрываем модальное окно.
+    CloseModal()
+}
+);
